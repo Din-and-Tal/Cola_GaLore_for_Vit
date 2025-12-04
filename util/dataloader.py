94f668f2d5a48b61ab43c5393afc15d2d199cf29
TalBenAmii <tal-ben@campus.technion.ac.il>
2025-12-04 22:28:36 +0200

    last com

--- util/dataloader.py
+++ util/dataloader.py
@@ -89,19 +89,19 @@ def get_data_loaders(cfg):
     train_indices = indices[:train_size]
     val_indices = indices[train_size : train_size + val_size]
     test_indices = indices[train_size + val_size :]
-    debug_batches = int(len(test_indices) * cfg.debug_data_scale)
+    debug_labels_count = int(len(test_indices) * cfg.debug_data_scale)
     # Reduce data if fullTrain is False
     if not cfg.full_train:
         train_indices = train_indices[
-            : max(1, debug_batches)
+            : max(1, debug_labels_count)
         ]
         val_indices = val_indices[
-            : max(1, debug_batches)
+            : max(1, debug_labels_count)
         ]
         test_indices = test_indices[
-            : max(1, debug_batches)
+            : max(1, debug_labels_count)
         ]
-        print(f"Using reduced data: {debug_batches} epoches")
+        print(f"Using reduced data: {debug_labels_count} epoches")
 
     # 4. Create Subsets
     # Train gets Augmented dataset, Val/Test get Clean dataset
