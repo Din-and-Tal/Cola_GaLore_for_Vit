e91ffec4ed2b2fcd00fd7c0f0bab71eb6c13f5d6
TalBenAmii <tal-ben@campus.technion.ac.il>
2025-12-07 19:26:03 +0200

    update

--- main/cola.sh
+++ main/cola.sh
@@ -12,6 +12,4 @@
     # "v2_cola_glora_layer"
 # )
 
-python main/main.py --config-name v1_cola_adamw wandb_project_name=cola_v1_no_amp_rank_search use_amp=false
 python main/main.py --config-name v2_cola_adamw wandb_project_name=cola_v2_rank_search
-python main/main.py --config-name v2_cola_adamw wandb_project_name=cola_v2_no_amp_rank_search use_amp=false
