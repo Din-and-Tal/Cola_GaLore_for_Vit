0b4f7401ff3a0e884ae284a1c1c03144bd799a37
TalBenAmii <tal-ben@campus.technion.ac.il>
2025-12-07 15:49:06 +0200

    a

--- main/cola.sh
+++ main/cola.sh
@@ -4,10 +4,10 @@ config_names=(
     # "vit_adamw"
     # "vit_glora"
     # "vit_glora_layer"
-    "v1_cola_adamw"
-    "v1_cola_glora"
-    "v1_cola_glora_layer"
-    # "v2_cola_adamw"
+    # "v1_cola_adamw"
+    # "v1_cola_glora"
+    # "v1_cola_glora_layer"
+    "v2_cola_adamw"
     # "v2_cola_glora"
     # "v2_cola_glora_layer"
 )
@@ -16,5 +16,5 @@ set -e # terminates consecutive calls when error occurs
 
 for config in "${config_names[@]}"; do
     echo "Running with config: $config"
-    python main/main.py --config-name $config wandb_project_name=cola_rank_search
+    python main/main.py --config-name $config wandb_project_name=cola_v2_rank_search
 done
